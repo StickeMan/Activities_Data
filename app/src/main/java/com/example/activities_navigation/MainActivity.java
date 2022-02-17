5ff10e189362b9e3856d4374eeb814f218626080
StickeMan <2005040@utrivieramaya.edu.mx>
2022-02-16 23:19:47 -0500

    Example2

--- app/src/main/java/com/example/activities_navigation/MainActivity.java
+++ app/src/main/java/com/example/activities_navigation/MainActivity.java
@@ -6,16 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button boton;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         boton = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
         boton.setOnClickListener(this);
     }
 
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.button:
                 Intent intent = new Intent(this, MainActivity2.class);
+                String data = editText.getText().toString();
+                intent.putExtra("DATO",data);
                 startActivity(intent);
                 break;
 
--- app/src/main/java/com/example/activities_navigation/MainActivity2.java
+++ app/src/main/java/com/example/activities_navigation/MainActivity2.java
@@ -2,13 +2,26 @@ package com.example.activities_navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        textView = (TextView) findViewById(R.id.text);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if (bundle!=null){
+            String cadena = (String) bundle.get("DATO");
+            textView.setText(cadena);
+        }
     }
 }
